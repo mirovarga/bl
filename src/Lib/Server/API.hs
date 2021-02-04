98bde999c8d841adc69f00eef29227a3901e539f
Miro Varga <me@mirovarga.com>
2021-02-04 21:38:21 +0100

    REST API: Support for fetching posts by index

--- src/Lib/Server/API.hs
+++ src/Lib/Server/API.hs
@@ -5,7 +5,7 @@ module Lib.Server.API (run) where
 
 import Control.Monad.IO.Class
 import Data.Maybe
-import Data.Text hiding (filter)
+import Data.Text hiding (filter, length)
 import Lib.Markdown
 import Lib.Post
 import qualified Network.Wai.Handler.Warp as W
@@ -14,14 +14,27 @@ import System.FilePath
 
 type PostsAPI =
   "posts" :> QueryParam "tag" Text :> Get '[JSON] [Post]
+    :<|> "posts" :> Capture "index" Int :> Get '[JSON] Post
     :<|> "posts" :> Capture "slug" Text :> Get '[JSON] Post
 
 server :: FilePath -> Server PostsAPI
-server dir = posts :<|> postWithKey
+server dir = posts :<|> postWithIndex :<|> postWithKey
   where
     posts :: Maybe Text -> Handler [Post]
     posts Nothing = liftIO posts'
     posts (Just t) = liftIO $ posts' >>= (return . withTag t)
+
+    postWithIndex :: Int -> Handler Post
+    postWithIndex i = do
+      post <- liftIO $ posts' >>= (return . lookup' i)
+      case post of
+        Nothing -> throwError err404
+        (Just p) -> return p
+      where
+        lookup' :: Int -> [Post] -> Maybe Post
+        lookup' index posts
+          | index >= 0 && index < length posts = Just (posts !! index)
+          | otherwise = Nothing
 
     postWithKey :: Text -> Handler Post
     postWithKey s = do
@@ -30,6 +43,7 @@ server dir = posts :<|> postWithKey
         Nothing -> throwError err404
         Just p -> return p
 
+    posts' :: IO [Post]
     posts' =
       newestFirst
         . filter (not . fromMaybe False . draft)
